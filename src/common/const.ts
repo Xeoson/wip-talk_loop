1554ff8f2df7ae717f1d76808f916c05d63d69e6
Andrey <Andrey9191919@yandex.ru>
2023-04-16 02:05:23 +0300

    i

--- src/common/const.ts
+++ src/common/const.ts
@@ -1,5 +1,7 @@
 
 export const BASE_URL =
-  process.env.NODE_ENV == "development"
+  process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://talk-loop.vercel.app";;
+    : "https://talk-loop.vercel.app";
+
+console.log('BASE_URL', BASE_URL)
