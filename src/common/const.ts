6028d8d1f768e5d865fec206edf1c9b0b86709f6
Andrey <Andrey9191919@yandex.ru>
2023-04-15 23:25:52 +0300

    i

---
+++ src/common/const.ts
@@ -0,0 +1,2 @@
+
+export const BASE_URL = "https://talk-loop.vercel.app";
--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -1,6 +1,7 @@
+import { BASE_URL } from "@/common/const";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export default new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: BASE_URL + "/api/graphql",
   cache: new InMemoryCache(),
 });
